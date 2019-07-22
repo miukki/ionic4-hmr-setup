9ed2633fca5b3dbfdc2a439c783b8f45cbc84f30
Anja Ishmukhametova <miukki@gmail.com>
2019-07-23 04:14:06 +1000

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  title: string = 'title';
+
+  update = (): void => this.title = 'updated!';
+
+  constructor() {
+
+  }
 
 }
